765b6a17ee704392816678e352d0a5b8e314896e
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-10-02 15:03:17 +0900

    feat: 배달의민족 홈 화면 AutoLayout 적용

--- iOS_Study_B/ViewController.swift
+++ iOS_Study_B/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerBackgroundView: UIView!
+    @IBOutlet weak var homeBoxView: UIView!
+    @IBOutlet weak var homeBoxViewSecond: UIView!
+    @IBOutlet weak var homeBoxViewThird: UIView!
+    @IBOutlet weak var homeBoxViewFourth: UIView!
+    @IBOutlet weak var homeBoxViewFifth: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        UIFont.familyNames.sorted().forEach { familyName in
+            print("*** \(familyName) ***")
+            UIFont.fontNames(forFamilyName: familyName).forEach { fontName in
+                print("\(fontName)")
+            }
+            print("---------------------")
+        }
+
+        headerBackgroundView.layer.cornerRadius = 25
+        homeBoxView.layer.cornerRadius = 15
+        homeBoxViewSecond.layer.cornerRadius = 15
+        homeBoxViewThird.layer.cornerRadius = 15
+        homeBoxViewFourth.layer.cornerRadius = 15
+        homeBoxViewFifth.layer.cornerRadius = 15
+
+
+        //원하는 모서리만 둥글게 만들기, 좌하단 우하단
+        headerBackgroundView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
     }
-
-
 }
 
