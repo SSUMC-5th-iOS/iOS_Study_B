818b3ec8c90869844ae5612baddc25047ebc8429
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2023-10-02 17:38:36 +0900

    week2_study

---
+++ iOS_Study_B/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Catstagram
+//
+//  Created by 정민지 on 2023/10/01.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    @IBAction func emailTextFeildEditingChanged(_ sender: UITextField) {
+        print("호출됨")
+    }
+    
+
+}
