e6cb142e61e32116ed4835cad41f6385d0127282
woonge0930 <woonge0930@naver.com>
2023-10-07 14:14:44 +0900

    ..

--- iOS_Study_B/ViewController.swift
+++ iOS_Study_B/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //aaaaaaa
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
