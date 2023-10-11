c4f060d72ef2d6d289a9dacc8a212d1ae0914dc5
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-10-11 18:25:40 +0900

    feat: protocol을 활용한 delegate 학습

--- iOS_Study_B/SecondViewController.swift
+++ iOS_Study_B/SecondViewController.swift
@@ -7,23 +7,25 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, ChangeLabelDelegate {
+
+    var previousViewController: ViewController?
+
+    @IBOutlet weak var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        previousViewController?.delegate = self
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonDismiss(_ sender: Any) {
+        dismiss(animated: true)
+        doChange()
     }
-    */
 
+    func doChange() {
+        previousViewController?.label.text = textField.text
+    }
 }
--- iOS_Study_B/ViewController.swift
+++ iOS_Study_B/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
+protocol ChangeLabelDelegate {
+    func doChange()
+}
+
 class ViewController: UIViewController {
+
+    var delegate: ChangeLabelDelegate?
+
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonGoNextVC(_ sender: Any) {
+        guard let nextVC = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController else {
+            return
+        }
 
+        nextVC.previousViewController = self
+
+        nextVC.modalPresentationStyle = .fullScreen
+        present(nextVC, animated: true)
+    }
 }
 
