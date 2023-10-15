739434f49491849a18f86c7fe3d67a981e6a4159
woongaaaa <woonge0930@naver.com>
2023-10-15 21:16:55 +0900

    Section6.24

--- iOS_Study_B/Home/Cell/StoryCollectionViewCell.swift
+++ iOS_Study_B/Home/Cell/StoryCollectionViewCell.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 class StoryCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet weak var viewimageViewBackground: UIView!
+    @IBOutlet weak var viewUserProfileBackground: UIView!
+    @IBOutlet weak var imageViewUserProfile: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        viewimageViewBackground.layer.cornerRadius=24
+        viewUserProfileBackground.layer.cornerRadius=23.5
+        imageViewUserProfile.layer.cornerRadius=22.5
+        imageViewUserProfile.clipsToBounds = true
+        
         // Initialization code
     }
 
--- iOS_Study_B/Home/Cell/StoryTableViewCell.swift
+++ iOS_Study_B/Home/Cell/StoryTableViewCell.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class StoryTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func setCollectionViewDataSourceDelegate(dataSourceDelegate : UICollectionViewDelegate & UICollectionViewDataSource, forRow row: Int)
+    {
+        collectionView.delegate = dataSourceDelegate
+        collectionView.dataSource = dataSourceDelegate
+        collectionView.tag = row
+        let storyNib = UINib(nibName: "StoryCollectionViewCell", bundle: nil)
+        collectionView.register(storyNib, forCellWithReuseIdentifier: "StoryCollectionViewCell")
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        flowLayout.minimumLineSpacing = 12
+        
+        collectionView.collectionViewLayout = flowLayout
+        collectionView.reloadData()
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
