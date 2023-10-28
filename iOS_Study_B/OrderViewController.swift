4a8936aa8b0ec4ce5fd08066cd7d4859552738e9
woongaaaa <woonge0930@naver.com>
2023-10-29 03:14:13 +0900

    주문확인화면 전환 구현 중

---
+++ iOS_Study_B/OrderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OrderViewController.swift
+//  iOS_Study_B
+//
+//  Created by 황채웅 on 2023/10/29.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.interactivePopGestureRecognizer?.delegate=nil
+
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func backButtonDidTap(_ sender: UIBarButtonItem) {
+        //뒤로가기
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iOS_Study_B/ViewController.swift
+++ iOS_Study_B/ViewController.swift
@@ -9,10 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Buttonnecessary: UIButton!
+    @IBOutlet weak var Buttonshowinfo: UIButton!
+    @IBOutlet weak var Buttonunnecessary: UIButton!
+    @IBOutlet weak var Buttontotalprice: UIButton!
+    
+    var largeIsActive:Bool = false
+    var mediumIsActive:Bool = false
+    var isActive:Bool = false
+    
+    @IBOutlet weak var buttonMedium: UIButton!
+    @IBOutlet weak var buttonLarge: UIButton!
+    @IBOutlet weak var buttonDough: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Buttonnecessary.layer.cornerRadius = 15
+        Buttonunnecessary.layer.cornerRadius = 15
+        Buttontotalprice.layer.cornerRadius = 4
+        
+        Buttonshowinfo.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
+        
     }
+    @IBAction func buttonMedium(_ sender: UIButton) {
+        
+        if mediumIsActive{
+        }else{
+            if(largeIsActive==true){
+                largeIsActive = false
+                buttonLarge.setImage(UIImage(named: "notselect"), for: .normal)
+            }
+            mediumIsActive = true
+            buttonMedium.setImage(UIImage(named: "select"), for: .normal)
+            
+            
+        }
+    }
+    @IBAction func buttonLarge(_ sender: UIButton) {
+        if largeIsActive{
+        }else{
+            if(mediumIsActive == true)
+            {
+                mediumIsActive = false
+                buttonMedium.setImage(UIImage(named: "notselect"), for: .normal)
+            }
+            largeIsActive = true
+            buttonLarge.setImage(UIImage(named:"select"), for: .normal)
+        }
+    }
+    @IBAction func buttonDough(_ sender: UIButton) {
+        if isActive{
+            isActive = false
+            buttonDough.setImage(UIImage(named: "select"), for: .normal)
+        }else{
+            isActive = true
+            buttonDough.setImage(UIImage(named:"allnotselect"), for: .normal)
+        }
+    }
+    @IBAction func orderButtonDidTap(_ sender: UIButton) {
+        // 1.스토리보드를 생성
+        let storyboard = UIStoryboard(name:"Main",bundle:nil)
+        // 2.뷰컨트롤러를 생성
+        let orderViewController = storyboard.instantiateViewController(identifier: "OrderVC") as! OrderViewController
+        // 3.화면전환 메소드를 이용하여 화면 전환
+        self.navigationController?.pushViewController(orderViewController, animated: true)
+    }
+    
 
 
 }
