14ba6af3d9573996df1923c51e03b359fb5676bd
ayoung1201 <banaabc@naver.com>
2023-10-30 19:22:38 +0900

    12

---
+++ iOS_Study_B/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  iOS_Study_B
+//
+//  Created by 임아영 on 2023/10/27.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableview.delegate = self
+        tableview.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+        
+    }
+
+ 
