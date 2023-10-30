c53590658cd0ed90b89c3c27fab64e2d091209c3
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2023-10-30 19:24:10 +0900

    ff

---
+++ iOS_Study_B/Home/HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.swift
+//  iOS_Study_B
+//
+//  Created by 정민지 on 2023/10/12.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        let feedNib = UINib(nibName: "FeedTableViewCell", bundle: nil)
+        tableView.register(feedNib, forCellReuseIdentifier: "FeedTableViewCell")
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
+extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 600
+    }
+}
