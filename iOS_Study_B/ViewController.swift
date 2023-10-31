0feb8bb687edde2ad1d47ef8de9b1333b0b382ec
woongaaaa <woonge0930@naver.com>
2023-10-31 12:11:53 +0900

    코드 연습

--- iOS_Study_B/ViewController.swift
+++ iOS_Study_B/ViewController.swift
@@ -6,13 +6,129 @@
 //
 
 import UIKit
+import SnapKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVAudioPlayerDelegate {
 
+    var player: AVAudioPlayer!
+    var timer: Timer!
+    
+    
+    let timeLabel: UILabel = {
+            let label = UILabel()
+            label.text = "00:00:00"
+            label.font = UIFont.systemFont(ofSize: 20)
+            label.textColor = .blue
+            
+            return label
+    }()
+    
+    let playButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named:"button_play"), for: .normal)
+        button.addTarget(self, action: #selector(playButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    let playSlider: UISlider = {
+        let slider = UISlider()
+        slider.minimumTrackTintColor = .red
+        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
+        return slider
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        configureSubviews()
+        makeConstraints()
+        initializePlayer()
     }
+    
+    func configureSubviews(){
+        view.addSubview(timeLabel)
+        view.addSubview(playButton)
+        view.addSubview(playSlider)
+    }
+    func makeConstraints() {
+        timeLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        playButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(timeLabel.snp.top).offset(-20)
+        }
+        playSlider.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(timeLabel.snp.bottom).offset(+20)
+        }
+    }
+    
+    @objc func playButtonTapped() {
+        playButton.isSelected.toggle()
+        playButton.setImage(UIImage(named:"button_pause"), for: .selected)
+        if playButton.isSelected {
+                    self.player?.play()
+                    self.makeAndFireTimer()
+                } else {
+                    self.player?.pause()
+                    self.invalidateTimer()
+                }
+    }
+    
+    @objc func sliderValueChanged() {
+        self.updateTimeLabelText(time: TimeInterval(playSlider.value))
+                if playSlider.isTracking { return }
+                self.player.currentTime = TimeInterval(playSlider.value)
+
+    }
+    
+    func initializePlayer() {
+            guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound") else {
+                print("음원 파일 에셋을 가져올 수 없습니다.")
+                return
+            }
+
+            do {
+                try self.player = AVAudioPlayer(data: soundAsset.data)
+                self.player.delegate = self
+            } catch let error as NSError {
+                print("플레이어 초기화 실패")
+                print("코드 : \(error.code), 메세지: \(error.localizedDescription)")
+            }
+
+            self.playSlider.maximumValue = Float(self.player.duration)
+            self.playSlider.minimumValue = 0
+            self.playSlider.value = Float(self.player.currentTime)
+        }
+
+        func updateTimeLabelText(time: TimeInterval) {
+            let minute: Int = Int(time/60)
+            let second: Int = Int(time.truncatingRemainder(dividingBy: 60))
+            let milisecond: Int = Int(time.truncatingRemainder(dividingBy: 1) * 100)
+            
+            let timeText: String = String(format: "%02ld:%02ld:%02ld", minute, second, milisecond)
+            
+            self.timeLabel.text = timeText
+        }
+
+        func makeAndFireTimer() {
+            self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [unowned self] (timer: Timer) in
+                
+                if self.playSlider.isTracking { return }
+                
+                self.updateTimeLabelText(time: self.player.currentTime)
+                self.playSlider.value = Float(self.player.currentTime)
+            })
+            self.timer.fire()
+        }
+
+        func invalidateTimer() {
+            self.timer.invalidate()
+            self.timer = nil
+        }
+    
 
 
 }
